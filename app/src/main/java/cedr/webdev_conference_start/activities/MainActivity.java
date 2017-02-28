a2313c7bab1708136458826474f189096355fe60
jraco <jraco11@gmail.com>
2017-02-27 22:06:02 -0800

    add packages.

--- app/src/main/java/cedr/webdev_conference_start/activities/MainActivity.java
+++ app/src/main/java/cedr/webdev_conference_start/activities/MainActivity.java
@@ -1,7 +1,9 @@
-package cedr.webdev_conference_start;
+package cedr.webdev_conference_start.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import cedr.webdev_conference_start.R;
 
 public class MainActivity extends AppCompatActivity {
 
